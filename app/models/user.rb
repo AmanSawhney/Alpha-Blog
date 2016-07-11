0398b962abccd1d80c5a94b253009931cf9889f0
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-11 15:06:19 -0400

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ActiveRecord::Base
+	validates :username, uniqueness: true, presence: true, length: {minimum: 3, maxium: 50}
+	validates :email, uniqueness: true, presence: true, email: true
 
 end
