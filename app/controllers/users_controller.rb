78238601d932ec05787527719d493b7ff980d6cd
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-12 14:01:08 -0400

    Added User Sign Up to the UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController 
+	def new
+		@user = User.new
+	end
+	def create 
+		@user = User.new(user_params)
+		if @user.save 
+			flash[:success] = "User was created"
+			redirect_to articles_path
+		else
+			render :new #validations failed
+		end
+	end
+	def user_params 
+			params.require(:user).permit(:username, :email, :password)
+	end
+end
