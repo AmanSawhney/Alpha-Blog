92c0fe0d6a725849ffdc6a3f4be90f343130d8e3
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-13 10:21:26 -0400

    Added users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 	def create 
 		@user = User.new(user_params)
 		if @user.save 
-			flash[:success] = "User was created"
+			flash[:success] = "Welcome to the alpha blog #{@user.username}"
 			redirect_to articles_path
 		else
 			render :new #validations failed
