56d39559c94c42c57b7b18ffc57a79c75472467a
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-13 10:59:02 -0400

    Added edit update func from GUI and Partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,20 @@ class UsersController < ApplicationController
 			render :new #validations failed
 		end
 	end
+	def edit 
+		@user= User.find(params[:id])
+	end
+
+	def update 
+		@user= User.find(params[:id])
+		if @user.update(user_params) 
+			flash[:success] = "#{@user.username} has been successfully updated!"
+			redirect_to articles_path
+		else
+			render :edit #validations failed
+		end
+	end
 	def user_params 
-			params.require(:user).permit(:username, :email, :password)
+		params.require(:user).permit(:username, :email, :password)
 	end
 end
