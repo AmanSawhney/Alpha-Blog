e74fab5bcba2b9ac8f0e9931197b7962cea1fbe8
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-14 10:48:00 -0400

    Added pages to the user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,11 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 	def index
-		@users = User.all
+		@users = User.paginate(:page => params[:page], :per_page => 5)
 	end
 	def create 
 		@user = User.new(user_params)
+		@user.username = @user.username.slice(0,1).capitalize + @user.username.slice(1..-1)
 		if @user.save 
 			flash[:success] = "Welcome to the alpha blog #{@user.username}"
 			redirect_to articles_path
@@ -30,7 +31,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-		@articles = Article.all
+		@articles = Article.paginate(:page => params[:page], :per_page => 5)
 	end
 	def user_params 
 		params.require(:user).permit(:username, :email, :password)
