393baaa079b1d50aa54b6c4616a4829293bb4049
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-25 11:34:22 -0400

    Changed nav bar based on user sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,19 +5,18 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user, :logged_in?
 
-
     def current_user
-      @current_user ||= User.find(session[:user_id] if session[:user_id]) #returns a user if the browser has a user in the session
+        @current_user ||= User.find(session[:user_id]) if session[:user_id] # returns a user if the browser has a user in the session
     end
 
     def logged_in?
-      ||current_user
+        !!current_user
     end
 
     def require_user
-      if !logged_in?
-        flash[:danger] = "You must be logged in to preform that action"
-        redirect_to root_path
-      end
+        unless logged_in?
+            flash[:danger] = 'You must be logged in to preform that action'
+            redirect_to root_path
+        end
     end
 end
