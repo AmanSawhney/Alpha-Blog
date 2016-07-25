7976555a33a8622e0d85fd186092643f010c9f3d
Gagandeep Sawhney <amansawhney@sawhneygames.com>
2016-07-25 11:36:38 -0400

    Used the before action to remove redunancy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:edit, :update, :show]
     def new
         @user = User.new
     end
@@ -19,11 +20,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-        @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:success] = "#{@user.username} has been successfully updated!"
             redirect_to articles_path
@@ -33,11 +32,14 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @articles = Article.paginate(page: params[:page], per_page: 5)
     end
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
